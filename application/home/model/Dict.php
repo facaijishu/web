7ed327c2cd12ea6bf496c7c4d4032029296905b4
RubyZheng0203 <zyc_mine2@163.com>
2019-11-20 11:02:36 +0800

    Edit

--- application/home/model/Dict.php
+++ application/home/model/Dict.php
@@ -6,5 +6,144 @@ use think\Model;
 
 class Dict extends Model
 {
+    //获取业务类型一级和二级标签
+    public function getService()
+    {
+        //一级
+        $dict    = $this->alias('d')
+                        ->join('DictType dt','d.dt_id=dt.dt_id')
+                        ->field('d.id,d.fid,d.value')
+                        ->where(['d.fid'=>0,'d.status'=>1,'dt.sign'=>'service_type'])
+                        ->order(['list_order'=>'desc'])
+                        ->select();
+        //二级
+        foreach ($dict as $key => $value) {
+            $dict2   = $this->alias('d')
+                            ->join('DictType dt','d.dt_id=dt.dt_id')
+                            ->field('d.id,d.fid,d.value')
+                            ->where(['d.fid'=>$value['id'],'d.status'=>1,'dt.sign'=>'service_type'])
+                            ->order(['list_order'=>'desc'])
+                            ->select();
+            if (!empty($dict2)) {
+                $dict[$key]['sub'] = $dict2;
+            }
+        }
+        return $dict;
+    }
+    
+    
+    //获取所属行业一级和二级标签
+    public function getIndustry()
+    {
+        $dict    = $this->alias('d')
+                        ->join('DictType dt','d.dt_id=dt.dt_id')
+                        ->field('d.id,d.fid,d.value')
+                        ->where(['d.fid'=>0,'d.status'=>1,'dt.sign'=>'industry'])
+                        ->order(['list_order'=>'desc'])
+                        ->select();
+ 
+        //二级
+        foreach ($dict as $key => $value) {
+            $dict2   = $this->alias('d')
+                            ->join('DictType dt','d.dt_id=dt.dt_id')
+                            ->field('d.id,d.fid,d.value')
+                            ->where(['d.fid'=>$value['id'],'d.status'=>1,'dt.sign'=>'industry'])
+                            ->order(['list_order'=>'desc'])
+                            ->select();
+            if (!empty($dict2)) {
+                $dict[$key]['sub'] = $dict2;
+            }
+        }
+        return $dict;
+    }
+    
+    //获取投融规模的标签
+    public function getSize()
+    {
+        $dict    = $this->alias('d')
+                        ->join('DictType dt','d.dt_id=dt.dt_id')
+                        ->field('d.id,d.value')
+                        ->where(['d.status'=>1,'dt.sign'=>'size'])
+                        ->order(['list_order'=>'desc'])
+                        ->select();
+        return $dict;
+    }
+    
+    //获取所在省份的标签
+    public function getProvince()
+    {
+        $dict    = $this->alias('d')
+                        ->join('DictType dt','d.dt_id=dt.dt_id')
+                        ->field('d.id,d.value')
+                        ->where(['d.status'=>1,'dt.sign'=>'to_province'])
+                        ->order(['list_order'=>'desc'])
+                        ->select();
+
+        return $dict;
+    }
+    
+    /**
+     * 获取标签一级集合
+     * @param string 标签二级字符串以逗号分割
+     * @return string|unknown
+     */
+    public function getTopDictList($id = '')
+    {
+        $sql   = "SELECT  id,`value` as dict_name from fic_dict where id in ( SELECT distinct  fid  from fic_dict where id in (". $id ."))";
+        $list  = $this->query($sql);
+
+        return $list;
+    }
+    
+    /**
+     * 获取标签二级集合
+     * @param string 行业二级字符串以逗号分割
+     * @return string|unknown
+     */
+    public function getSecDictList($id = '')
+    {
+        $sql   = "SELECT  id,`value` as dict_name from fic_dict where id in  (". $id .")";
+        $list  = $this->query($sql);
+        
+        return $list;
+    }
+    
+    /**
+     * 获取标签名称字符串
+     * @param string $id
+     * @return string|unknown
+     */
+    public function getDictStr($id = '')
+    {
+        $result  = $this->alias("d")
+                        ->field('d.id,d.value')
+                        ->where(" id in (" .$id." ) ")
+                        ->select();
+        $str = '';
+        foreach ($result as $key => $item) {
+            if($str==''){
+                $str  = $item['value'];
+            }else{
+                $str  = $str."、".$item['value'];
+            }
+        }
+        return $str;
+    }
+    
+    
+    /**
+     * 获取标签二级集合
+     * @param string 行业二级字符串以逗号分割
+     * @return string|unknown
+     */
+    public function getDictList($id = '')
+    {
+        
+        $result  = $this->alias("d")
+                        ->field('d.id ,d.value as dict_name')
+                        ->where(" id = " .$id)
+                        ->find();
+        return $result;
+    }
     
 }
--- application/home/model/PcConsultation.php
+++ application/home/model/PcConsultation.php
@@ -6,7 +6,7 @@ use think\Model;
 
 class PcConsultation extends Model
 {
-    public function getPcConsultationList($length = 0){
+    public function getPcConsultationList($length = 4){
         if($length === 0){
             $result = $this->alias('con')
                     ->join("dict d" , 'con.type = d.id')
@@ -27,9 +27,68 @@ class PcConsultation extends Model
         foreach ($result as $key => $value) {
             $url = $model->getImgInfoById($value['top_img']);
             $result[$key]['top_img_url'] = 'http://fin.jrfacai.com/uploads/'.$url['url'];
+            $result[$key]['short_des']   = subStrLen($value['des'],60);
         }
         return $result;
     }
+    
+    
+    /**
+     * 获取项目列表
+     * @param 当前页数 $page
+     * @return number[]|string[]|NULL[]|unknown[]
+     */
+    public function getList($page){
+        $return       = [];
+        $length       = 20;
+        if($page==1){
+            $start    = 0;
+        }else{
+            $start    = ($page-1)*$length;
+        }
+        
+        $year    = date( "Y",time())-1;
+        $month   = date( "M",time());
+        $day     = date( "d",time());
+        $time    = strtotime( $year."-"."$month"."-".$day." 00:00:00");
+        $list    = $this->alias('con')
+                        ->join("dict d" , 'con.type = d.id')
+                        ->where(' con.status = 1 and con.create_time > '.$time)
+                        ->field("SQL_CALC_FOUND_ROWS con.*,d.value")
+                        ->order(" con.is_hot desc ,con.con_id desc ")
+                        ->limit($start, $length)->select();
+        
+        $result = $this->query('SELECT FOUND_ROWS() as count');
+        $total  = $result[0]['count'];
+        
+        $model = model("MaterialLibrary");
+        foreach ($list as $key => $value) {
+            $url = $model->getImgInfoById($value['top_img']);
+            $list[$key]['top_img_url']  = config('material_img.upload_root').$url['url'];
+            $list[$key]['short_des']    = subStrLen($value['des'],50);
+            $list[$key]['item_url']     = "/home/consultation/index/id/".$value['con_id'];
+            $list[$key]['style_bg_url'] = "background: url('".config('material_img.upload_root').$url['url']."')  center no-repeat;";
+        }
+        
+        if(empty($list)){
+            $return['code']            = 201;
+            $return['msg']             = "列表没有数据";
+            $return['data']            = "";
+            $return['page']            = 0;
+            $return['ptotal']          = 0;
+        }else{
+            
+            $return['code']            = 200;
+            $return['msg']             = "";
+            $return['data']            = $list;
+            $return['page']            = ceil($total/$length);
+            $return['ptotal']          = $total;
+        }
+        return $return;
+    }
+    
+    
+    
     public function getPcConsultationInfoById($id){
         $result = $this->where(['con_id' => $id])->find();
         return $result;
