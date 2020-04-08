17271ec921060d2ea6375a6c5cc84d141f60a3f1
RubyZheng0203 <zyc_mine2@163.com>
2020-04-08 14:29:43 +0800

    Add 三方演绎

---
+++ application/home/controller/Trilateral.php
@@ -0,0 +1,36 @@
+<?php
+namespace app\home\controller;
+
+class Trilateral extends Base
+{
+    public function index()
+    {
+        $model = model("Trilateral");
+        $list  = $model->getList(4);
+        
+        $this->assign('list' , $list);
+        $this->assign('title' , "三方演绎——FA財");
+        return view();
+    }
+    
+    public function detail()
+    {
+        $id    = $this->request->param('id/d');
+        
+        $model = model("Trilateral");
+        $info  = $model->getInfo($id);
+        $nlist = $model->getNearList($id,4);
+        
+        $pro   = model("Project");
+        $list  = $pro->getProjectShowDs($info['pro_ids']);
+        
+        
+        
+        $this->assign('project' , $list);
+        $this->assign('nlist' , $nlist);
+        $this->assign('info' , $info);
+        $this->assign('title' , $info['title']."｜FA財-一站式智能信息投融交互平台");
+        return view();
+    }
+}
+
